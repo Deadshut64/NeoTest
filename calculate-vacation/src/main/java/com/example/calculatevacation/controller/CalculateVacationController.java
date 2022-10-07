66c27acfc00cd7eee061aaf81e07835e243088b6
Deadshut64 <sergeychuchmanov@gmail.com>
2022-10-07 21:52:32 +0400

    correct controller

--- calculate-vacation/src/main/java/com/example/calculatevacation/controller/CalculateVacationController.java
+++ calculate-vacation/src/main/java/com/example/calculatevacation/controller/CalculateVacationController.java
@@ -1,16 +1,21 @@
 package com.example.calculatevacation.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @RestController
 @RequestMapping("/")
 public class CalculateVacationController {
 
+    @GetMapping
 public double calculate(
-        @RequestParam("salary") double salary;
-        @RequestParam("vacation") int vacation;
+        @RequestParam("salary") double salary,
+        @RequestParam("vacation") int vacation
  ){
     return salary / 29.3 * vacation;
 }
