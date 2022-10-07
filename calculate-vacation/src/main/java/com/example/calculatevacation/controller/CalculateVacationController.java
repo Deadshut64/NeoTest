5b1a2b86ec452b6edf89666c2050fd69a1956db4
Deadshut64 <sergeychuchmanov@gmail.com>
2022-10-07 21:48:02 +0400

    add controller

--- calculate-vacation/src/main/java/com/example/calculatevacation/controller/CalculateVacationController.java
+++ calculate-vacation/src/main/java/com/example/calculatevacation/controller/CalculateVacationController.java
@@ -1,4 +1,17 @@
 package com.example.calculatevacation.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class CalculateVacationController {
+
+public double calculate(
+        @RequestParam("salary") double salary;
+        @RequestParam("vacation") int vacation;
+ ){
+    return salary / 29.3 * vacation;
+}
 }
